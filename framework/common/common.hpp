d45a99b440941fbbd14f2c45df33d612916e07a6
su <z69500jqk@qq.com>
2022-05-17 17:36:58 +0800

    调整依赖库

---
+++ framework/common/common.hpp
@@ -0,0 +1,13 @@
+﻿//
+// Created by 秋鱼 on 2022/5/11.
+//
+
+#pragma once
+
+#ifndef DEL_COPY_IN_CLASS
+#define DEL_COPY_IN_CLASS(CLASS_NAME)                         \
+        CLASS_NAME(const CLASS_NAME&) = delete;               \
+        CLASS_NAME(CLASS_NAME&&) = delete;                    \
+        CLASS_NAME& operator=(const CLASS_NAME&) = delete;    \
+        CLASS_NAME& operator=(CLASS_NAME && )= delete;        
+#endif
