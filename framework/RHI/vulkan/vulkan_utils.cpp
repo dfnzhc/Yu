c48744887b1cc873de4510c175412e1465d24ed6
su <z69500jqk@qq.com>
2022-06-18 17:21:16 +0800

    两个 vulkan 相关的工具函数

--- framework/RHI/vulkan/vulkan_utils.cpp
+++ framework/RHI/vulkan/vulkan_utils.cpp
@@ -75,4 +75,65 @@ VkShaderModule LoadShader(std::string_view fileName, VkDevice device)
     return shaderModule;
 }
 
+uint32_t SizeOfFormat(VkFormat format)
+{
+    switch (format) {
+        case VK_FORMAT_R8_SINT:
+            return 1;//(BYTE)
+        case VK_FORMAT_R8_UINT:
+            return 1;//(UNSIGNED_BYTE)1
+        case VK_FORMAT_R16_SINT:
+            return 2;//(SHORT)2
+        case VK_FORMAT_R16_UINT:
+            return 2;//(UNSIGNED_SHORT)2
+        case VK_FORMAT_R32_SINT:
+            return 4;//(SIGNED_INT)4
+        case VK_FORMAT_R32_UINT:
+            return 4;//(UNSIGNED_INT)4
+        case VK_FORMAT_R32_SFLOAT:
+            return 4;//(FLOAT)
+
+        case VK_FORMAT_R8G8_SINT:
+            return 2 * 1;//(BYTE)
+        case VK_FORMAT_R8G8_UINT:
+            return 2 * 1;//(UNSIGNED_BYTE)1
+        case VK_FORMAT_R16G16_SINT:
+            return 2 * 2;//(SHORT)2
+        case VK_FORMAT_R16G16_UINT:
+            return 2 * 2; // (UNSIGNED_SHORT)2
+        case VK_FORMAT_R32G32_SINT:
+            return 2 * 4;//(SIGNED_INT)4
+        case VK_FORMAT_R32G32_UINT:
+            return 2 * 4;//(UNSIGNED_INT)4
+        case VK_FORMAT_R32G32_SFLOAT:
+            return 2 * 4;//(FLOAT)
+
+        case VK_FORMAT_UNDEFINED:
+            return 0;//(BYTE) (UNSIGNED_BYTE) (SHORT) (UNSIGNED_SHORT)
+        case VK_FORMAT_R32G32B32_SINT:
+            return 3 * 4;//(SIGNED_INT)4
+        case VK_FORMAT_R32G32B32_UINT:
+            return 3 * 4;//(UNSIGNED_INT)4
+        case VK_FORMAT_R32G32B32_SFLOAT:
+            return 3 * 4;//(FLOAT)
+
+        case VK_FORMAT_R8G8B8A8_SINT:
+            return 4 * 1;//(BYTE)
+        case VK_FORMAT_R8G8B8A8_UINT:
+            return 4 * 1;//(UNSIGNED_BYTE)1
+        case VK_FORMAT_R16G16B16A16_SINT:
+            return 4 * 2;//(SHORT)2
+        case VK_FORMAT_R16G16B16A16_UINT:
+            return 4 * 2;//(UNSIGNED_SHORT)2
+        case VK_FORMAT_R32G32B32A32_SINT:
+            return 4 * 4;//(SIGNED_INT)4
+        case VK_FORMAT_R32G32B32A32_UINT:
+            return 4 * 4;//(UNSIGNED_INT)4
+        case VK_FORMAT_R32G32B32A32_SFLOAT:
+            return 4 * 4;//(FLOAT)
+    }
+
+    return 0;
+}
+
 } // namespace yu::vk
--- framework/RHI/vulkan/vulkan_utils.hpp
+++ framework/RHI/vulkan/vulkan_utils.hpp
@@ -10,5 +10,17 @@ namespace yu::vk {
 
 VkShaderStageFlagBits GetShaderType(std::string_view fileName);
 VkShaderModule LoadShader(std::string_view fileName, VkDevice device);
+uint32_t SizeOfFormat(VkFormat format);
+
+template<typename T>
+requires std::same_as<decltype(T::sType), VkStructureType>
+bool EntityNotSet(T t)
+{
+    if (t.sType == VK_STRUCTURE_TYPE_APPLICATION_INFO) {
+        return true;
+    }
+    
+    return false;
+}
 
 } // namespace yu::vk
