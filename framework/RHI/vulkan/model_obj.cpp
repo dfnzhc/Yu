c765c39a7f814e649a1b2aebe6ac8e9cc84a54bf
su <z69500jqk@qq.com>
2022-07-11 23:29:31 +0800

    简单的 obj 模型加载

---
+++ framework/RHI/vulkan/model_obj.cpp
@@ -0,0 +1,115 @@
+﻿//
+// Created by 秋鱼 on 2022/7/8.
+//
+
+#include <logger.hpp>
+#include "model_obj.hpp"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+#include <common/common.hpp>
+
+namespace std {
+template<>
+struct ::std::hash<yu::vk::VertexObj>
+{
+    size_t operator()(yu::vk::VertexObj const& vertex) const
+    {
+        return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.normal) << 1)) >> 1)
+            ^ (hash<glm::vec2>()(vertex.uv) << 1);
+    }
+};
+}
+
+namespace yu::vk {
+
+void ModelObj::load(std::string_view fileName, std::string_view basePath, bool triangulate)
+{
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn;
+    std::string err;
+    auto fullPath = yu::GetModelFile(std::string{basePath.data()} + fileName.data());
+    bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err,
+                                fullPath.c_str(),
+                                yu::GetModelFile(basePath.data()).c_str(), triangulate);
+
+    if (!err.empty()) {
+        LOG_ERROR("{}", err);
+    }
+
+    if (!ret) {
+        LOG_FATAL("Failed to load/parse .obj file: {}.", fileName);
+    }
+
+    std::unordered_map<VertexObj, uint32_t> uniqueVertices{};
+    for (const auto& shape : shapes) {
+        for (const auto& index : shape.mesh.indices) {
+            VertexObj vertex{};
+
+            vertex.pos = {
+                attrib.vertices[3 * index.vertex_index + 0],
+                attrib.vertices[3 * index.vertex_index + 1],
+                attrib.vertices[3 * index.vertex_index + 2]
+            };
+
+            vertex.normal = {
+                attrib.normals[3 * index.normal_index + 0],
+                attrib.normals[3 * index.normal_index + 1],
+                attrib.normals[3 * index.normal_index + 2]
+            };
+
+            if (!attrib.texcoords.empty()) {
+                vertex.uv = {
+                    attrib.texcoords[2 * index.texcoord_index + 0],
+                    1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
+                };
+            }
+
+            auto mat = std::find_if(materials.begin(), materials.end(), [&shape](const tinyobj::material_t& mat)
+            {
+                return mat.name == shape.name;
+            });
+            
+            vertex.color = {
+                mat->diffuse[0], mat->diffuse[1], mat->diffuse[2]
+            };
+
+            if (uniqueVertices.count(vertex) == 0) {
+                uniqueVertices[vertex] = static_cast<uint32_t>(obj_.vertices.size());
+                obj_.vertices.push_back(vertex);
+            }
+
+            obj_.indices.push_back(uniqueVertices[vertex]);
+        }
+    }
+}
+
+void ModelObj::setPipelineVertexInput(std::vector<VkVertexInputBindingDescription>& bindingDesc,
+                                      std::vector<VkVertexInputAttributeDescription>& attrDesc)
+{
+    bindingDesc = {VertexObj::getBindingDescription()};
+    attrDesc = VertexObj::getAttributeDescriptions();
+}
+
+void ModelObj::allocMemory(StaticBuffer& staticBuffer)
+{
+    staticBuffer.allocBuffer(static_cast<uint32_t>(obj_.vertices.size()),
+                             sizeof(VertexObj),
+                             obj_.vertices.data(),
+                             &vertex_info_);
+
+    staticBuffer.allocBuffer(static_cast<uint32_t>(obj_.indices.size()),
+                             sizeof(uint32_t),
+                             obj_.indices.data(),
+                             &index_info_);
+}
+
+void ModelObj::draw(VulkanPipeline& pipeline, VkCommandBuffer cmdBuffer, VkDescriptorBufferInfo* pConstantBuffer, VkDescriptorSet descriptorSet)
+{
+    pipeline.drawIndexed(cmdBuffer, static_cast<uint32_t>(obj_.indices.size()),
+                         &vertex_info_, &index_info_, pConstantBuffer, descriptorSet);
+}
+
+} // yu::vk
---
+++ framework/RHI/vulkan/model_obj.hpp
@@ -0,0 +1,96 @@
+﻿//
+// Created by 秋鱼 on 2022/7/8.
+//
+
+#pragma once
+
+#include "static_buffer.hpp"
+#include "pipeline.hpp"
+
+#include <glm/gtx/hash.hpp>
+
+namespace yu::vk {
+
+// 简单实现的 obj 模型类，用于加载一些只有顶点颜色的模型，纹理需要额外设置
+struct VertexObj
+{
+    glm::vec3 pos;
+    glm::vec3 normal;
+    glm::vec2 uv;
+
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription()
+    {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(VertexObj);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        return bindingDescription;
+    }
+
+    static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions()
+    {
+        std::vector<VkVertexInputAttributeDescription> attributeDescriptions(4);
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(VertexObj, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(VertexObj, normal);
+
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(VertexObj, uv);
+
+        attributeDescriptions[3].binding = 0;
+        attributeDescriptions[3].location = 3;
+        attributeDescriptions[3].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[3].offset = offsetof(VertexObj, color);
+
+        return attributeDescriptions;
+    }
+
+    bool operator==(const VertexObj& other) const
+    {
+        return pos == other.pos && normal == other.normal && uv == other.uv && color == other.color;
+    }
+};
+
+struct ModelDataObj
+{
+    std::vector<VertexObj> vertices;
+    std::vector<uint32_t> indices;
+};
+
+class ModelObj
+{
+public:
+    ModelObj() = default;
+    ~ModelObj() = default;
+
+    void load(std::string_view fileName, std::string_view basePath = "", bool triangulate = true);
+
+    void setPipelineVertexInput(std::vector<VkVertexInputBindingDescription>& bindingDesc,
+                                std::vector<VkVertexInputAttributeDescription>& attrDesc);
+
+    void allocMemory(StaticBuffer& staticBuffer);
+
+    void draw(VulkanPipeline& pipeline,
+              VkCommandBuffer cmdBuffer,
+              VkDescriptorBufferInfo* pConstantBuffer,
+              VkDescriptorSet descriptorSet);
+
+private:
+    ModelDataObj obj_;
+    VkDescriptorBufferInfo vertex_info_{};
+    VkDescriptorBufferInfo index_info_{};
+};
+
+} // yu::vk
